ce5dc871db914670b03f979d4a8813da05448e9d
Kayden <ohaocong@gmail.com>
2022-11-10 20:04:42 +0800

    Second Commit

--- csv_exercise.py
+++ csv_exercise.py
@@ -4,11 +4,15 @@ import statistics
 
 
 # Variables
+# Update csv_file to target CSV File name.
+# Ensure the csv file is in current working directory.
+# Update col_dict base on the possible Columns Types
 csv_file = 'addresses.csv'
 csv_path = f'{os.getcwd()}\{csv_file}'
 col_dict = {'Postal': 0, 'Name': 1, 'Address': 2}
 
 
+# Takes in a String and return the possible Column Type
 def typeCheck(val: str):
     col_types = list(col_dict.keys())
 
@@ -20,15 +24,18 @@ def typeCheck(val: str):
 
     # Contains both numberic and alpha, conclude Address.
     else: val_type = col_types[2]
-    
+
     return val_type
 
+# Takes in an Item and use col_dict to returns it's Key. 
 def getKey(val):
     for key,item in col_dict.items():
         if item == val:
             return key
 
 def main():
+
+    # Update range base on the number of columns in CSV File.
     count = [[] for i in range(6)]
     df = pd.read_csv(csv_path)
     for idx, row in df.iterrows():
